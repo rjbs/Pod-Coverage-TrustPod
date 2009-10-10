207da3c21ed40ad6748eab94c89424c60bfaf3d0
Andreas Marienborg <andreas.marienborg@gmail.com>
2009-10-10 23:03:12 +0200

    Added failing test for base classes with no pod in them

---
+++ t/eg/ChildWithPod.pm
@@ -0,0 +1,20 @@
+package ChildWithPod;
+
+use base qw(BaseWithNoPod);
+
+=head1 NAME
+
+ChildWithPod - And we inherit from someone without it, how rude!
+
+
+=head1 METHODS
+
+=head2 blah
+
+Blah method
+
+=cut
+
+sub blah {}
+
+1;
